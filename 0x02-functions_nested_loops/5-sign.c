9baed4855c98c576e5850ae76c307aefee923ada
faith-wanjiku <caps3703@gmail.com>
2022-09-15 16:25:34 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_sign -prints sign of number
+ * print_sign - print sign of number
  * @n: number to check 
  * Return: 1 if greater than 0 , 0 if 0, -1 if less than 0
  */
