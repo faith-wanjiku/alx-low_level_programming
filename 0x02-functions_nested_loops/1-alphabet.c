327c0c60ffe8173823907f387d464c2b86fe1454
faith-wanjiku <caps3703@gmail.com>
2022-09-15 11:34:52 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ *   main -check the code
+ *
+ *   Return:Always 0.
+ */
+int main(void)
+{
+	char alpha;
+
+	alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	-putchar('\n');
+}
+
