099ec675651fd09f75b09f5c5a2c19a2c9a55c22
faith-wanjiku <caps3703@gmail.com>
2022-09-15 15:26:22 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints lowercase ten times followed by new line
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int counter;
+
+	for (counter = 1; counter <= 10; counter++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
+}
+
