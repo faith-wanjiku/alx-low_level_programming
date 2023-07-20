e0761f156159ef0631471d1c01daa6ff94ee4c12
faith-wanjiku <caps3703@gmail.com>
2023-07-20 14:25:01 -0700

    salute

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_number - print any integer using putchar
+ * @n: integer to be printed
+ */
+
+void print_number(int n)
+{
+	int count, x, y;
+
+	x = 0;
+	y = 1000000000;
+
+	if (n == 0)
+		_putchar('0');
+	else if (n < 0)
+	{
+		n *= -1;
+		_putchar('-');
+	}
+	for (count = 0; count < 10; count++)
+	{
+		if (n / y == 0 && x == 0)
+		{
+			y /= 10;
+			continue;
+		}
+		else if (x == 0)
+		{
+			_putchar((n / y) + '0');
+			x += 1;
+		}
+		else
+			_putchar(((n / y) % 10) + '0');
+		y /= 10;
+	}
+}
